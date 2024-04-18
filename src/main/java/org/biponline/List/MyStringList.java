bd803600c2dd84e0a457feeeb5b4e210fa2b52a5
Krapivin <tailsforhire229@gmail.com>
2024-04-18 14:01:37 +0300

    Start - 18.04

--- src/main/java/org/biponline/List/MyStringList.java
+++
@@ -1,4 +0,0 @@
-package org.biponline.List;
-
-public class MyStringList {
-}
---
+++ src/main/java/org/biponline/MainIntList.java
@@ -0,0 +1,43 @@
+package org.biponline;
+
+import org.biponline.List.MyIntegerArrayList;
+import java.util.Arrays;
+public class MainIntList {
+
+    public static void main(String[] args) {
+        MyIntegerArrayList list = new MyIntegerArrayList(3);
+
+        // Testing add(Integer item) method
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println("List after adding elements: " + Arrays.toString(list.array));
+
+        // Testing add(int index, Integer item) method
+
+        list.add(1, 10);
+        System.out.println("List after adding element at index 1: " + Arrays.toString(list.array));
+
+        // Testing set(int index, Integer item) method
+
+        list.set(2, 20);
+        System.out.println("List after setting element at index 2: " + Arrays.toString(list.array));
+
+        // Testing remove(Integer item) method
+
+        list.remove(0); // Delete 10 and type 0 for stabling work
+        System.out.println("List after removing element 10: " + Arrays.toString(list.array));
+
+        // Testing remove(int index) method
+
+        list.remove(0);
+        System.out.println("List after removing element at index 0: " + Arrays.toString(list.array));
+
+        // Testing contains(Integer item) method
+
+        Integer item = 20;
+        System.out.println("List contains element 20: " + list.contains(item));
+    }
+}
